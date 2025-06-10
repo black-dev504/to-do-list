33b91ea674a3ff0bb11bee8ba371afe450373eb3
usman sy <blackdev504@gmail.com>
2025-06-10 14:38:17 -0700

    v2

--- src/components/toDo.vue
+++ src/components/toDo.vue
@@ -1,21 +1,35 @@
 <template>
   <div class="bg-[#2E2E2E] min-w-[300px] p-5 rounded-lg flex flex-col gap-5 justify-between">
-    <h1 class="text-[#E8E8E8] font-bold text-2xl">{{ category }}</h1>
+    <div class="flex justify-between items-center">
+      <h1 class="text-[#E8E8E8] font-bold text-2xl">{{ category }}</h1>
+    </div>
 
     <div class="flex flex-col gap-3">
       <div
         v-for="(task, index) in tasks"
         :key="index"
-        class="rounded-lg flex justify-between items-center"
+        class="rounded-lg flex justify-between items-center w-full"
       >
-        <div class="flex items-center justify-center gap-3">
+        <div class="flex items-center justify-center gap-3 w-full">
           <input
+            @click="task.completed = !task.completed"
+            v-model="task.completed"
             type="checkbox"
-            class="appearance-none checked:bg-[#9E78CF] w-4 h-4 cursor-pointer border-1 border-[#9E78CF] bg-[#2E2E2E] rounded-sm"
-            v-model="clicked"
-            @click="clicked = !clicked"
+            class="appearance-none checked:bg-[#a09f9f] w-4 h-4 cursor-pointer border-1 border-[#9E78CF] bg-[#2E2E2E] rounded-sm"
           />
-          <p class="text-[#9E78CF]">{{ task }}</p>
+          <div class="flex justify-between items-center w-full">
+            <p
+              :class="[
+                'w-full text-sm',
+                checkedTasks.includes(index) ? 'line-through text-[#a09f9f]' : 'text-[#9E78CF]',
+              ]"
+            >
+              {{ task.text }}
+            </p>
+            <button @click="deleteTask(task.id)">
+              <img src="/icons/deleteicon.svg" class="w-3 h-3 cursor-pointer" alt="" />
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -29,11 +43,16 @@
       />
     </div>
     <div class="flex justify-between items-center">
-      <button @click="$emit('add-task', newTask)">
+      <button @click="addTask">
         <img class="w-5 h-5 cursor-pointer" src="/icons/addicon.svg" alt="" />
       </button>
       <button>
-        <img class="w-5 h-5 cursor-pointer" src="/icons/deleteicon.svg" alt="" />
+        <img
+          @click="removeCategory"
+          class="w-5 h-5 cursor-pointer"
+          src="/icons/deleteicon.svg"
+          alt=""
+        />
       </button>
     </div>
   </div>
@@ -53,10 +72,33 @@ export default {
   },
   data() {
     return {
-      clicked: false,
+      newTask: '',
+      checkedTasks: [],
     }
+  },
+  methods: {
+    removeCategory() {
+      this.$emit('remove-category', this.category)
+    },
+
+    addTask() {
+      if (this.newTask.trim() !== '') {
+        this.$emit('add-task', this.category, this.newTask)
+        this.newTask = ''
+      }
+    },
+
+    deleteTask(index) {
+      this.$emit('delete-task', this.category, index)
+    },
+
+    toggleChecked(index) {
+      if (this.checkedTasks.includes(index)) {
+        this.checkedTasks = this.checkedTasks.filter((i) => i !== index)
+      } else {
+        this.checkedTasks.push(index)
+      }
+    },
   },
 }
 </script>
-
-<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,7 @@
 <template>
-  <section class="bg-[#171717] min-h-screen">
+  <section class="bg-[#171717] min-h-screen primary-font">
     <div class="bg-[#171717] px-5 lg:px-20 py-15">
       <div class="flex w-6/10 mx-auto gap-4">
-        <!-- <div class="relative overflow-hidden w-full min-h-30">/ -->
         <div class="flex h-12 items-center mb-10 group border-b-1 border-[#3E1671] w-full">
           <input
             @click="toggleInput"
@@ -12,20 +11,9 @@
             v-model="category"
           />
         </div>
-        <!-- <div
-            v-if="clicked"
-            class="transition-transform h-12 duration-700 border-b-1 border-[#3E1671] absolute w-full mb-10 top-16"
-          >
-            <input
-              placeholder="Tasks"
-              class="text-white px-3 py-3 h-full w-full flex focus:outline-0"
-              type="text"
-              v-model="currentList.tasks"
-            />
-          </div> -->
-        <!-- </div> -->
+
         <button
-          @click="addTask"
+          @click="addCategory"
           class="text-white flex gap-4 justify-center w-10 h-10 bg-[#9E78CF] items-center rounded-lg cursor-pointer"
         >
           <img src="/icons/plus.svg" class="w-5 h-5" alt="" />
@@ -36,7 +24,13 @@
     <div class="px-5 lg:px-20">
       <div class="flex gap-5">
         <div v-for="(list, index) in toDoList" :key="index">
-          <toDo :category="list.category" :tasks="list.tasks" />
+          <toDo
+            :category="list.category"
+            :tasks="list.tasks"
+            @add-task="addTask"
+            @delete-task="deleteTask"
+            @remove-category="removeCategory"
+          />
         </div>
       </div>
     </div>
@@ -52,7 +46,9 @@ export default {
       clicked: false,
 
       category: '',
-      tasks: '',
+      tasks: [],
+      totalTasks: 0,
+      completedTasks: 0,
 
       toDoList: [],
     }
@@ -64,15 +60,33 @@ export default {
     toggleInput() {
       this.clicked = true
     },
-    addTask() {
+    addCategory() {
       const exists = this.toDoList.some((list) => list.category === this.category)
       if (!exists && this.category.trim() !== '') {
         this.toDoList.push({
           category: this.category,
-          tasks: ['read book', 'write code', 'exercise'],
+          tasks: [],
         })
         this.category = ''
-        console.log(this.toDoList)
+      }
+    },
+
+    removeCategory(category) {
+      this.toDoList = this.toDoList.filter((list) => list.category !== category)
+    },
+
+    addTask(category, newTask) {
+      const currentList = this.toDoList.find((list) => list.category === category)
+      if (currentList) {
+        currentList.tasks.push({ id: Date.now() + Math.random(), text: newTask, completed: false })
+      }
+    },
+
+    deleteTask(category, index) {
+      const currentList = this.toDoList.find((list) => list.category === category)
+
+      if (currentList && currentList.tasks[index]) {
+        currentList.tasks.splice(index, 1)
       }
     },
   },
